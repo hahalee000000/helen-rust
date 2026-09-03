5f5743a696ca6fa012b66c1d01d65027a4f795a9
hahalee000000 <15865216+hahalee000000@user.noreply.gitee.com>
2026-09-03 17:30:09 +0800

    M25.18: force-add frontend dist assets to git for CI

---
+++ crates/helen-agent/frontend/dist/assets/swimlanesDiagram-VK2B7HYN-DZiG5a6Z.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-A5DVABFB-CwhLT8d8.js";import{_ as a}from"./index-C21LdUR2.js";import"./chunk-5VM5RSS4-CkoyT0zc.js";import"./chunk-XXDRQBXY-BETINMKi.js";import"./chunk-POPQ4Y6H-DwfY5Vbf.js";import"./chunk-F27PBJKO-CTFmM9WG.js";import"./channel-OHIL3G4p.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
